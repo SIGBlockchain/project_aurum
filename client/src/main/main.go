930b4c2267d0fb0e3599dde13d96d1ad075bd7b8
jldistefano <jldstefano@gmail.com>
2019-02-20 14:40:20 -0600

    Added Client Online Check

--- client/src/main/main.go
+++ client/src/main/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	client "project_aurum/client/src/client"
+	)
+
+// Initializes logger format
+func init() {
+	log.SetPrefix("LOG: ")
+	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Lshortfile)
+}
+
+func main() {
+	// Check to see if there is an internet connection
+	err := client.CheckConnection()
+	if err != nil {
+		log.Fatalln("Connectivity check failed.")
+	}
+	log.Println("Connection check passed.")
 }
