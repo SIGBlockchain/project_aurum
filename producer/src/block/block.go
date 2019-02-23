c65013fb8105cee21f400583a55084ecaae77b9f
ramirez915 <jose.angelramirez915@gmail.com>
2019-02-23 14:51:08 -0600

    Block Struct
    current block struct

--- producer/src/block/block.go
+++ producer/src/block/block.go
@@ -1,1 +1,11 @@
 package block
+
+//data a block contains
+type Block struct {
+	version        uint32 // 4 bytes
+	height         uint64 // 8 bytes
+	previousHash   []byte // 32bytes
+	merkleRootHash []byte
+	timeStamp      uint64
+	data           [][]byte // byte size tbd
+}
