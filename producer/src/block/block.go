6b383b7603bdaf9b18f9e79bea570a45bea6852c
ramirez915 <jose.angelramirez915@gmail.com>
2019-02-24 00:52:09 -0600

    Serialize with tests. Block Header only
    Serialize function for block header information

--- producer/src/block/block.go
+++ producer/src/block/block.go
@@ -1,5 +1,7 @@
 package block
 
+import "encoding/binary" // for converting to uints to byte slices
+
 type Block struct {
 	Version        uint32
 	Height         uint64
@@ -7,4 +9,29 @@ type Block struct {
 	MerkleRootHash []byte
 	TimeStamp      int64
 	Data           [][]byte
+}
+
+// function only serializes the block header for now
+// need to add in the Data
+func (b *Block) Serialize() []byte {
+	// slice that will only contain the Version and Height (size = 12)
+	slice1 := make([]byte, 12)
+	// Version and Height to bytes in little endian and inside slice1
+	binary.LittleEndian.PutUint32(slice1[0:4], b.Version)
+	binary.LittleEndian.PutUint64(slice1[4:12], b.Height)
+
+	// both hashes together
+	hashSlices := append(b.PreviousHash, b.MerkleRootHash...)
+
+	// concat what is available so far Version, Height, PreviousHash and MerkleRootHash
+	slice1Nhashes := append(slice1, hashSlices...)
+
+	//TimeStamp to bytes in little endian and inside ist own slice
+	timeSlice := make([]byte, 8)
+	binary.LittleEndian.PutUint64(timeSlice[0:8], uint64(b.TimeStamp))
+
+	// now concatenate all of the block header pieces
+	slice1NhashesTimeSlice := append(slice1Nhashes, timeSlice...)
+
+	return slice1NhashesTimeSlice
 }
--- producer/src/block/block_test.go
+++ producer/src/block/block_test.go
@@ -1,1 +1,74 @@
 package block
+
+import (
+	"bytes"           // for comparing []bytes
+	"encoding/binary" // for encoding/decoding
+	"fmt"             // printing
+	"testing"         // testing
+	"time"            // to get time stamp
+)
+
+func TestSerialize(t *testing.T) {
+	// get time stamp
+	ti := time.Now()
+	nowTime := ti.UnixNano()
+
+	// create the block
+	b := Block{
+		Version:        3,
+		Height:         300,
+		PreviousHash:   []byte{'g', 'u', 'a', 'v', 'a'},
+		MerkleRootHash: []byte{'g', 'r', 'a', 'p', 'e'},
+		TimeStamp:      nowTime,
+		Data:           [][]byte{}, // need to add
+	}
+	// now use the serialize function
+	serial := b.Serialize()
+	// will keep track of current position in the slice
+	pos := 0
+	fmt.Printf("serial... %x\n", serial)
+
+	// check version
+	fmt.Printf("b.Version %x\n", b.Version)
+	fmt.Printf("spot for version %d\n", binary.LittleEndian.Uint32(serial[0:4]))
+	blockVersion := binary.LittleEndian.Uint32(serial[0:4])
+
+	if blockVersion != b.Version {
+		t.Errorf("version does not match")
+	}
+
+	// check height
+	fmt.Printf("b.Height %d\n", b.Height)
+	fmt.Printf("spot for height bytes %d\n", binary.LittleEndian.Uint32(serial[4:12]))
+	blockHeight := binary.LittleEndian.Uint64(serial[4:12])
+	if blockHeight != b.Height {
+		t.Errorf("height does not match")
+	}
+	pos = 12
+
+	// check previousHash
+	fmt.Printf("b.previousHash %x\n", b.PreviousHash)
+	fmt.Printf("spot for previousHash bytes %x\n", serial[12:pos+len(b.PreviousHash)])
+	blockPrevHash := serial[12 : pos+len(b.PreviousHash)]
+	if bytes.Equal(blockPrevHash, b.PreviousHash) != true {
+		t.Errorf("Previous hashes do not match\n")
+	}
+	pos = pos + len(b.PreviousHash)
+
+	//check merkleRootHash
+	fmt.Printf("b.merkleRootHash %x\n", b.MerkleRootHash)
+	fmt.Printf("spot for merkleRootHash bytes %x\n", serial[pos:pos+len(b.MerkleRootHash)])
+	blockMerkleHash := serial[pos : pos+len(b.MerkleRootHash)]
+	if bytes.Equal(blockMerkleHash, b.MerkleRootHash) != true {
+		t.Errorf("Merkle hashes do not match\n")
+	}
+	pos = pos + len(b.MerkleRootHash)
+
+	//check timeStamp
+	fmt.Printf("b.TimeStamp %x\n", b.TimeStamp)
+	fmt.Printf("spot for timestamp bytes %x\n", binary.LittleEndian.Uint64(serial[pos:]))
+	blockTimeStamp := binary.LittleEndian.Uint64(serial[pos:])
+	if int64(blockTimeStamp) != b.TimeStamp {
+		t.Errorf("time stamps do not match")
+	}
+}
