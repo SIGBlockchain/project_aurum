9c04e6c098a8f96d08a2ba9524faf7ff82c64559
ramirez915 <jose.angelramirez915@gmail.com>
2019-02-23 23:33:21 -0600

    Serialize and Block struct changes
    Worked on the serialize function and its test. Also made changes to the struct

--- producer/src/block/block.go
+++ producer/src/block/block.go
@@ -1,11 +1,77 @@
 package block
 
-//data a block contains
+import (
+	"encoding/binary" // for converting to uints to byte slices
+)
+
 type Block struct {
-	version        uint32 // 4 bytes
-	height         uint64 // 8 bytes
-	previousHash   []byte // 32bytes
-	merkleRootHash []byte
-	timeStamp      uint64
-	data           [][]byte // byte size tbd
+	Version        uint32 //32/8 = 4 bytes
+	Height         uint64 // 64/8 = 8 bytes
+	PreviousHash   []byte // 32bytes 255 bits
+	MerkleRootHash []byte // 32bytes
+	TimeStamp      int64
+	Data           [][]byte
+}
+
+func (b *Block) Serialize() []byte {
+	// slice that will only contain the Version and Height (size = 12)
+	slice1 := make([]byte, 12)
+	// Version and Height to bytes in little endian and inside newSlice
+	binary.LittleEndian.PutUint32(slice1[0:4], b.Version)
+	binary.LittleEndian.PutUint64(slice1[4:12], b.Height)
+
+	// slice that will contain both hashes that are already in byte slices with remaining zeros of previous hash then merkleRoot
+	//hashSlice := make([]byte, 64)
+	hashSlice1 := make([]byte, 32-len(b.PreviousHash))
+	hashSlice1 = append(b.PreviousHash, hashSlice1...)
+	//fmt.Printf("hashSlice1 %x\n", hashSlice1)
+
+	hashSlice2 := make([]byte, 32-len(b.MerkleRootHash))
+	hashSlice2 = append(b.MerkleRootHash, hashSlice2...)
+	//	fmt.Printf("hashSlice2 %x\n", hashSlice2)
+
+	// both hashes together
+	//hashSlice = append(b.PreviousHash, b.MerkleRootHash...)
+	hashSlices := append(hashSlice1, hashSlice2...)
+	//	fmt.Print(b.PreviousHash)
+	//	fmt.Println(b.MerkleRootHash)
+	//	fmt.Printf("hashSlice %d\n", hashSlices)
+
+	// concat what we currently have which will be of size 76 bytes
+	// Version, Height, PreviousHash and MerkleRootHash
+	//slice1Nhash := make([]byte, 76)
+	slice1Nhashes := append(slice1, hashSlices...)
+
+	actualSliceNhashes := make([]byte, 76-len(slice1Nhashes))
+	actualSliceNhashes = append(slice1Nhashes, actualSliceNhashes...)
+
+	//TimeStamp to bytes in little endian and inside another slice
+	timeSlice := make([]byte, 8)
+	//binary.LittleEndian.PutUint64(timeSlice, uint64(b.TimeStamp))
+	binary.LittleEndian.PutUint64(timeSlice[0:8], uint64(b.TimeStamp))
+
+	// now get to the data
+	var dataSlice []byte // will store data from Data
+	// for each sub slice in Data
+	for i := 0; i < len(b.Data); i++ {
+		//fmt.Println(b.Data[i])
+		//fmt.Printf("len of b.Data[i] %d val of start %d\n", len(b.Data[i]), start)
+		//fmt.Printf("lower lim %d upper lim %d\n", start, start+len(b.Data[i]))
+		for x := 0; x < len(b.Data[i]); x++ {
+			dataSlice = append(dataSlice, []byte{b.Data[i][x]}...)
+		}
+	}
+	//fmt.Println(dataSlice)
+
+	//slice1HashTimeSlice := make([]byte, 84)
+	//slice1HashTimeSlice = append(slice1Nhashes, timeSlice...)
+	slice1NhashesTimeSlice := append(slice1Nhashes, timeSlice...)
+	actualSliceHashTime := make([]byte, 84-len(slice1NhashesTimeSlice))
+	actualSliceHashTime = append(slice1NhashesTimeSlice, actualSliceHashTime...)
+
+	// now concatenate all
+	var allData []byte
+	//allData = append(slice1HashTimeSlice, dataSlice...)
+	allData = append(actualSliceHashTime, dataSlice...)
+	return allData
 }
--- producer/src/block/block_test.go
+++ producer/src/block/block_test.go
@@ -1,1 +1,74 @@
 package block
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestSerialize(t *testing.T) {
+	// get time stamp
+	ti := time.Now()
+	nowTime := ti.UnixNano()
+	//fmt.Printf("time %d \n", nowTime)
+
+	// create the block header
+	b := Block{
+		Version:        3,
+		Height:         300,
+		PreviousHash:   []byte{'g', 'u', 'a', 'v', 'a'},
+		MerkleRootHash: []byte{'g', 'r', 'a', 'p', 'e'},
+		TimeStamp:      nowTime,
+		Data:           [][]byte{{'1', '2'}, {'3', '4', '5'}, {'9'}},
+	}
+	// now use the serialize function
+	serial := b.Serialize()
+	fmt.Printf("serial... %x\n", serial)
+	fmt.Printf("b.Version %x\n", b.Version)
+	fmt.Printf("spot 0-4 bytes %d\n", binary.LittleEndian.Uint32(serial[0:4]))
+
+	fmt.Printf("b.Height %d\n", b.Height)
+	fmt.Printf("spot 4-12 bytes %d\n", binary.LittleEndian.Uint32(serial[4:12]))
+
+	fmt.Printf("b.previousHash %x\n", b.PreviousHash)
+	fmt.Printf("spot 12-44 bytes %x\n", serial[12:44])
+
+	fmt.Printf("b.merkleRootHash %x\n", b.MerkleRootHash)
+	fmt.Printf("spot 44-76 bytes %x\n", serial[44:76])
+
+	fmt.Printf("b.TimeStamp %x\n", b.TimeStamp)
+	fmt.Printf("spot 76-84 bytes %x\n", binary.LittleEndian.Uint64(serial[76:84]))
+
+	fmt.Printf("b.data %x\n", b.Data)
+	fmt.Printf("spot 84: bytes %x\n", serial[84:])
+
+	tb := Block{}
+	r := bytes.NewBuffer(serial)
+	err := binary.Read(r, binary.LittleEndian, &tb.Version)
+	if err != nil {
+		fmt.Println("binary.Read failed:", err)
+	}
+	fmt.Println(tb.Version)
+
+	err = binary.Read(r, binary.LittleEndian, &tb.Height)
+	if err != nil {
+		fmt.Println("binary.Read failed:", err)
+	}
+	fmt.Println(tb.Height)
+
+	// reset r for the timestamp slice
+	r = bytes.NewBuffer(serial[76:84])
+	err = binary.Read(r, binary.LittleEndian, &tb.TimeStamp)
+	fmt.Println(tb.TimeStamp)
+	fmt.Println(b.TimeStamp)
+
+	// the above method does not work with []bytes/[][]bytes for some odd reason
+
+	/*
+		/*if bytes.Equal(b.PreviousHash, serial[12:44]) != true {
+			t.Errorf("Blocks' previouHash not the same as in the Serialize output\n")
+		}
+	*/
+}
