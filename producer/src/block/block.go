0b1a86d6706211571e40fc63e00a0d46a109f3d5
ramirez915 <jose.angelramirez915@gmail.com>
2019-02-24 01:53:30 -0600

    Changes to Serialize and Test
    Made changes to Serialize and updated the Test to match the changes

--- producer/src/block/block.go
+++ producer/src/block/block.go
@@ -5,33 +5,24 @@ import "encoding/binary" // for converting to uints to byte slices
 type Block struct {
 	Version        uint32
 	Height         uint64
+	Timestamp      int64
 	PreviousHash   []byte
 	MerkleRootHash []byte
-	TimeStamp      int64
 	Data           [][]byte
 }
 
 // function only serializes the block header for now
 // need to add in the Data
 func (b *Block) Serialize() []byte {
-	// slice that will only contain the Version and Height (size = 12)
-	slice1 := make([]byte, 12)
-	// Version and Height to bytes in little endian and inside slice1
-	binary.LittleEndian.PutUint32(slice1[0:4], b.Version)
-	binary.LittleEndian.PutUint64(slice1[4:12], b.Height)
+	// allocates space for the known variables
+	serializedBlock := make([]byte, 20)
 
-	// both hashes together
-	hashSlices := append(b.PreviousHash, b.MerkleRootHash...)
+	// convert the known variables to byte slices in little endian and add to slice
+	binary.LittleEndian.PutUint32(serializedBlock[0:4], b.Version)
+	binary.LittleEndian.PutUint64(serializedBlock[4:12], b.Height)
+	binary.LittleEndian.PutUint64(serializedBlock[12:20], uint64(b.Timestamp))
 
-	// concat what is available so far Version, Height, PreviousHash and MerkleRootHash
-	slice1Nhashes := append(slice1, hashSlices...)
-
-	//TimeStamp to bytes in little endian and inside ist own slice
-	timeSlice := make([]byte, 8)
-	binary.LittleEndian.PutUint64(timeSlice[0:8], uint64(b.TimeStamp))
-
-	// now concatenate all of the block header pieces
-	slice1NhashesTimeSlice := append(slice1Nhashes, timeSlice...)
-
-	return slice1NhashesTimeSlice
+	// now append the remaining information and return the complete block header byte slice
+	serializedBlock = append(serializedBlock, b.PreviousHash...)
+	return append(serializedBlock, b.MerkleRootHash...)
 }
