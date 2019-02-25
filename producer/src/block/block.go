9ae16b31efb47dffd77a540855fb33337f440e64
Csando6 <43687528+Csando6@users.noreply.github.com>
2019-02-24 23:16:11 -0600

    Update block.go
    added recursion and appending elements, need to add hashing method

--- producer/src/block/block.go
+++ producer/src/block/block.go
@@ -1,6 +1,10 @@
 package block
 
-import "encoding/binary" // for converting to uints to byte slices
+import ("encoding/binary" // for converting to uints to byte slices
+	//"crypto/sha256" //hashing library:has not been implemented yet
+	"fmt"//debugging purposes
+       )
+	
 
 type Block struct {
 	Version        uint32
@@ -25,4 +29,31 @@ func (b *Block) Serialize() []byte {
 	// now append the remaining information and return the complete block header byte slice
 	serializedBlock = append(serializedBlock, b.PreviousHash...)
 	return append(serializedBlock, b.MerkleRootHash...)
+}
+
+func appendString(A string, B string)string{ //accepts two strings, returns single string
+	A += B
+	return A
+}
+func recursiveAdd(myArray[] string)string{
+	if(len(myArray)==0){//if empty slice is passed
+		return ""
+	}else if (len(myArray) == 1){//end of function
+		return myArray[0]
+	}
+	tempArray := []string{} //will be used to append to
+
+	if(len(myArray)%2 == 1){ //if array is of odd length, makes it even
+		myArray = append(myArray,myArray[len(myArray)-1] ) //append last element to self to make it even
+	}
+	
+	for i:=0;i<len(myArray);i=i+2{
+		tempArray = append(tempArray, appendString(myArray[i],myArray[i+1] ))//calls append, which returns a single string
+	}
+	if(false){//debugging code, used to print out element
+		for i:=0;i<len(tempArray);i++{
+			fmt.Println(tempArray[i])
+		}
+	}
+	return recursiveAdd(tempArray) //recursion, returns single string
 }
