64f281314874fb610ea897807f50ae066907338c
jldistefano <jldstefano@gmail.com>
2019-03-01 21:17:28 -0600

    Added GetKeys()

--- client/src/keys/keys.go
+++ client/src/keys/keys.go
@@ -1,2 +1,43 @@
 package keys
 
+import (
+	"encoding/json"
+	"math/big"
+	"io/ioutil"
+	"os"
+)
+
+/*=================================================================================================
+* Purpose: Reads public and private keys from a json file, converts to big.Int                    *
+* Parameters: filename, a string containing the filename for the json file. usually               *
+*  ./.keys.dat.json                                                                               *
+* Returns: private key, a big.Int and public key, a big.Int                                       *
+=================================================================================================*/
+func GetKeys(filename string) (*big.Int, *big.Int) {
+	// Reads json file into b_string, if any errors occur, abort
+	if b_string, err := ioutil.ReadFile(filename); err == nil {
+		// Create message variable to hold json data
+		type keyStruct struct {
+			Private string
+			Public string
+		}
+		var keys keyStruct
+		// Load json data from text file
+		err = json.Unmarshal(b_string, &keys)
+
+		// Sets priv_key to a new big int, and converts sting from json file into big int
+		private_key := new(big.Int)
+		private_key.SetString(keys.Private, 16)
+
+		// Sets pub_key to a new big int, and converts sting from json file into big int
+		public_key := new(big.Int)
+		public_key.SetString(keys.Public, 16)
+
+		// Returns pub_key and priv_key
+		return private_key, public_key
+	} else {
+		// Fatal exit, placeholder
+		os.Exit(-1)
+		return big.NewInt(-1), big.NewInt(-1)
+	}
+}
--- producer/src/keys/keys_test.go
+++ producer/src/keys/keys_test.go
@@ -1,1 +1,38 @@
 package keys
+
+import (
+	"testing"
+	"os"
+	"math/big"
+)
+
+func TestGetKeys(t *testing.T) {
+	// Checks if fatal exit when file does not exist. WILL CRASH PROGRAM
+	//priv_key, pub_key := GetKeys("./.keys.test.dat.json")
+
+	// Creates test file, inserts test values
+	f, create_err := os.Create("./.keys.test.dat.json")
+    if create_err != nil {
+        t.Errorf("Failure in creating test file.")
+    }
+    _, write_err := f.WriteString(`{"private" : "0000000000000008","public" : "000000000000000f"}`)
+    if write_err != nil {
+        t.Errorf("Failure in writing test file.")
+    }
+    // Fills expected private/public values
+    expectedPrivate := new(big.Int)
+    expectedPublic := new(big.Int)
+    expectedPrivate.SetString("0000000000000008", 16)
+    expectedPublic.SetString("000000000000000f", 16)
+
+    // Gets actual private/public keys
+    priv_key, pub_key := GetKeys("./.keys.test.dat.json")
+    // Asserts if public and private keys are equal to their expected values
+    if pub_key.Cmp(expectedPublic) != 0 {
+    	t.Errorf("Public Key %d does not match expected key %d.", pub_key, expectedPublic)
+    }
+    if priv_key.Cmp(expectedPrivate) != 0{
+    	t.Errorf("Private Key %d does not match expected key %d.", priv_key, expectedPrivate)
+    }
+    os.Remove("./.keys.test.dat.json")
+}
