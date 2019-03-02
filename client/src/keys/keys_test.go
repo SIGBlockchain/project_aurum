1e55af8ca0301b2e1d42af3a6e2619c7e98946b6
jldistefano <jldstefano@gmail.com>
2019-03-02 14:10:11 -0600

    Fixed Keys Test

--- client/src/keys/keys_test.go
+++ client/src/keys/keys_test.go
@@ -12,7 +12,7 @@ func TestKeys(t *testing.T) {
 	// expected keys
 	expectedPrivKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 
-	testFile := "keys.dat"
+	testFile := "./keys.dat"
 
 	// This should open the file and store the keys in string form
 	err := StoreKey(expectedPrivKey, testFile)
@@ -25,8 +25,15 @@ func TestKeys(t *testing.T) {
     if err != nil {
         t.Errorf("Failed to retrieve keys")
     } 
-    if actualPrivKey == expectedPrivKey {
-        t.Errorf("Contents of file does not match the expected Private Key.")
+
+    actualPublicKey := actualPrivKey.PublicKey
+    expectedPublicKey := expectedPrivKey.PublicKey
+
+    if actualPrivKey.D.Cmp(expectedPrivKey.D) != 0 {
+    	t.Errorf("Private Key from file does not match expected Private Key.")
+    }
+    if actualPublicKey.X.Cmp(expectedPublicKey.X) != 0 && actualPublicKey.Y.Cmp(expectedPublicKey.Y) != 0 {
+        t.Errorf("Public Key from file does not match expected Public Key.")
     }
 
 	// Delete testFile
