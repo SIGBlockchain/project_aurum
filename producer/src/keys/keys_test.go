11ea11ecf987e0f5852b719e75ea30ee848f764c
jldistefano <jldstefano@gmail.com>
2019-03-02 13:22:27 -0600

    Duplicated client key code for producer

--- producer/src/keys/keys_test.go
+++ producer/src/keys/keys_test.go
@@ -1,1 +1,34 @@
 package keys
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"os"
+	"testing"
+)
+
+func TestKeys(t *testing.T) {
+	// expected keys
+	expectedPrivKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+
+	testFile := "./keys.dat"
+
+	// This should open the file and store the keys in string form
+	err := StoreKey(expectedPrivKey, testFile)
+	if err != nil {
+		t.Errorf("Failed to store keys.")
+	}
+
+    // actual key
+    actualPrivKey, err := GetKey(testFile)
+    if err != nil {
+        t.Errorf("Failed to retrieve keys")
+    } 
+    if actualPrivKey == expectedPrivKey {
+        t.Errorf("Contents of file does not match the expected Private Key.")
+    }
+
+	// Delete testFile
+	os.Remove(testFile)
+}
