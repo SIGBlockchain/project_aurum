86eefceee75392397f2eee18367e7c1c3356cd58
arosso3 <rossoklakovich@gmail.com>
2019-03-01 18:52:59 -0600

    Update run_all_tests.sh

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -2,7 +2,3 @@
 
 # Run all go test files in project directory
 go test ../...
-
-# Prompts for user input when done 
-# (this is so that the console stays open so that user can read any error messages)
-read -p "Press Return to Close..."
