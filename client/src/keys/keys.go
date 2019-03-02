c8ee2ed679ae327aeb957f60bffcb6e063e9d469
karol <kstola2@uic.edu>
2019-03-01 23:58:36 -0600

    Created unit test

--- client/src/keys/keys.go
+++ client/src/keys/keys.go
@@ -1,2 +1,27 @@
 package keys
 
+import (
+	"crypto/ecdsa"
+	"errors"
+)
+
+func StoreKey(p *ecdsa.PrivateKey, filename string) error {
+	// TODO
+
+
+
+
+
+	e := errors.New("Error") // change to meaningful text
+	return e
+}
+
+func GetKey(filename string) *ecdsa.PrivateKey, error {
+	// TODO
+
+
+
+
+	e := errors.New("Error") // change to meaningful text
+	return privateKey, e
+}
