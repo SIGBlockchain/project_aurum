c9d7202120f30eb485c5bee5e0fe391c2273dc5c
karol <kstola2@uic.edu>
2019-03-02 22:35:25 -0600

    Added tests

--- producer/src/blockchain/blockchain.go
+++ producer/src/blockchain/blockchain.go
@@ -2,22 +2,44 @@ package blockchain
 
 import block "../block"
 
-func AddBlock(b block.Block, filename string) error {
+// Phase 1:
+// Serializes the block
+// Gets size of the block string
+// Prepends the size of the block to the serialized block
+// Takes the resulting concatenation and writes it to file named filename
+// If the write fails, return an error
 
+// Phase 2:
+// Store the height, file position, size, and hash of the block into a database row
+// If this fails, return an error
+func AddBlock(b block.Block, filename string) error { // Additional parameter is DB connection
+	// TODO
 	return nil
 }
 
-func GetBlockByHeight(height int, filename string) (block.Block, error) {
-
-	return block.Block{}, nil
+// Phase 2:
+// Given a height number, opens the file filename and extracts the block of that height
+// Use a database query to find block's position and size in the file
+// If this fails, return an error
+func GetBlockByHeight(height int, filename string) ([]byte, error) { // Additional parameter is DB connection
+	// TODO
+	return []byte{}, nil
 }
 
-func GetBlockByIndex(index, filename string) (block.Block, error) {
-
-	return block.Block{}, nil
+// Phase 2:
+// Given a file position, opens the file filename and extracts the block at that position
+// Use a database query to find block's position and size in the file
+// If this fails, return an error
+func GetBlockByPosition(position int, filename string) ([]byte, error) { // Additional parameter is DB connection
+	// TODO
+	return []byte{}, nil
 }
 
-func GetBlockByHash(hash []byte, filename string) (block.Block, error) {
-
-	return block.Block{}, nil
+// Phase 2:
+// Given a block hash, opens the file filename and extracts the block that matches that block's hash
+// Use a database query to find block's position and size in the file
+// If this fails, return an error
+func GetBlockByHash(hash []byte, filename string) ([]byte, error) { // Additional parameter is DB connection
+	// TODO
+	return []byte{}, nil
 }
--- producer/src/blockchain/blockchain_test.go
+++ producer/src/blockchain/blockchain_test.go
@@ -1,1 +1,100 @@
 package blockchain
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	block "../block"
+)
+
+func TestAddBlockPhaseOne(t *testing.T) {
+	// Create a block
+	b := block.Block{
+		Version:        1,
+		Height:         0,
+		Timestamp:      time.Now().UnixNano(),
+		PreviousHash:   block.HashSHA256([]byte{'0'}),
+		MerkleRootHash: block.HashSHA256([]byte{'1'}),
+		Data:           [][]byte{block.HashSHA256([]byte{'x'})},
+	}
+	testFile := "testBlockchain.dat"
+	// Add the block
+	err := AddBlock(b, testFile)
+	if err != nil {
+		t.Errorf("Failed to add block")
+	}
+}
+
+func TestGetBlockByHeight(t *testing.T) {
+	// Create a block
+	expectedBlock := block.Block{
+		Version:        1,
+		Height:         0,
+		Timestamp:      time.Now().UnixNano(),
+		PreviousHash:   block.HashSHA256([]byte{'0'}),
+		MerkleRootHash: block.HashSHA256([]byte{'1'}),
+		Data:           [][]byte{block.HashSHA256([]byte{'x'})},
+	}
+	testFile := "testBlockchain.dat"
+	// Add the block
+	err := AddBlock(expectedBlock, testFile)
+	if err != nil {
+		t.Errorf("Failed to add block.")
+	}
+	actualBlock, err := GetBlockByHeight(0, testFile)
+	if err != nil {
+		t.Errorf("Failed to extract block.")
+	}
+	if reflect.DeepEqual(expectedBlock.Serialize(), actualBlock) == false {
+		t.Errorf("Blocks do not match")
+	}
+}
+func TestGetBlockPosition(t *testing.T) {
+	// Create a block
+	expectedBlock := block.Block{
+		Version:        1,
+		Height:         0,
+		Timestamp:      time.Now().UnixNano(),
+		PreviousHash:   block.HashSHA256([]byte{'0'}),
+		MerkleRootHash: block.HashSHA256([]byte{'1'}),
+		Data:           [][]byte{block.HashSHA256([]byte{'x'})},
+	}
+	testFile := "testBlockchain.dat"
+	// Add the block
+	err := AddBlock(expectedBlock, testFile)
+	if err != nil {
+		t.Errorf("Failed to add block.")
+	}
+	actualBlock, err := GetBlockByPosition(0, testFile)
+	if err != nil {
+		t.Errorf("Failed to extract block.")
+	}
+	if reflect.DeepEqual(expectedBlock.Serialize(), actualBlock) == false {
+		t.Errorf("Blocks do not match")
+	}
+}
+func TestGetBlockByHash(t *testing.T) {
+	// Create a block
+	expectedBlock := block.Block{
+		Version:        1,
+		Height:         0,
+		Timestamp:      time.Now().UnixNano(),
+		PreviousHash:   block.HashSHA256([]byte{'0'}),
+		MerkleRootHash: block.HashSHA256([]byte{'1'}),
+		Data:           [][]byte{block.HashSHA256([]byte{'x'})},
+	}
+	testFile := "testBlockchain.dat"
+	// Add the block
+	err := AddBlock(expectedBlock, testFile)
+	if err != nil {
+		t.Errorf("Failed to add block.")
+	}
+	actualBlock, err := GetBlockByHash(block.HashSHA256(expectedBlock.Serialize()), testFile)
+	if err != nil {
+		t.Errorf("Failed to extract block.")
+	}
+	if reflect.DeepEqual(expectedBlock.Serialize(), actualBlock) == false {
+		t.Errorf("Blocks do not match")
+	}
+}
