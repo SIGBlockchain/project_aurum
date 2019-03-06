6a15f3a904788d22854670e236bc81d0354a2ecf
jldistefano <jldstefano@gmail.com>
2019-03-06 13:52:56 -0600

    Added Command Line Arg Test

--- client/src/client/client_test.go
+++ client/src/client/client_test.go
@@ -28,3 +28,69 @@ func TestGetUserInput(t *testing.T) {
 		t.Errorf("User Input Check Failed.")
 	}
 }
+
+// Test simulates user input, ensures correct collection of command line arguments
+func TestReadCmdLineArgs(t *testing.T) {
+	var testread bytes.Buffer
+
+	// Empty Case
+	testread.Write([]byte(""))
+	slice_empty, err_empty := readCmdLineArgs(&testread)
+	if len(slice_empty) > 0 {
+		t.Errorf("Empty input yielded nonempty arguments, Test Failed.")	
+	}
+	if err_empty == nil {
+		t.Errorf("Empty input yielded no errors, Test Failed.")
+	}
+
+	// Singleton, just executable call, Case
+	testread.Reset()
+	testread.Write([]byte("./client"))
+	slice_single, err_single := readCmdLineArgs(&testread)
+	if len(slice_single) > 0 {
+		t.Errorf("Single input yielded nonempty arguments, Test Failed.")	
+	}
+	if err_single != nil {
+		t.Errorf("Single input yielded an error, Test Failed.")
+	}
+
+	// Other Case
+	testread.Reset()
+	testread.Write([]byte("./client -test -arguments"))
+	slice_other, err_other := readCmdLineArgs(&testread)
+	expected_args := {"-test", "-arguments"}
+	if slice_other != expected_args {
+		t.Errorf("Normal input yielded incorrect arguments, Test Failed.")	
+	}
+	if err_other != nil {
+		t.Errorf("Normal input yielded an error, Test Failed.")
+	}
+}
+
+// Test simulates user input, ensures correct processing of command line arguments
+func TestProcessCmdLineArgs(t *testing.T) {
+	var testread bytes.Buffer
+
+	// Empty Case
+	testread.Write([]byte(""))
+	err_empty := ProcessCmdLineArgs(&testread)
+	if err_empty == nil {
+		t.Errorf("Empty input yielded no errors, Test Failed.")
+	}
+
+	// Singleton, just executable call, Case
+	testread.Reset()
+	testread.Write([]byte("./client"))
+	err_single := ProcessCmdLineArgs(&testread)
+	if err_single != nil {
+		t.Errorf("Single input yielded an error, Test Failed.")
+	}
+
+	// Other Case
+	testread.Reset()
+	testread.Write([]byte("./client -test"))
+	err_other := ProcessCmdLineArgs(&testread)
+	if err_other == nil {
+		t.Errorf("Invalid input yielded no error, Test Failed.")
+	}
+}
