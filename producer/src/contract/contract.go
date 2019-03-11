7872fd1e87ef6f59d2c90fbf86ca32adddfa1d07
karol <kstola2@uic.edu>
2019-03-11 17:55:58 -0500

    Removed irrelevant functions/tests

--- producer/src/contract/contract.go
+++ producer/src/contract/contract.go
@@ -2,94 +2,29 @@ package contract
 
 import (
 	"crypto/ecdsa"
-	"database/sql"
-	"errors"
 )
 
 /*Contains a 32 size byte slice recipient,
 and a uint64 value */
 type Yield struct{}
 
-/*
-Consists of a previous contract hash,
-a claimed yield index,
-a public key,
-and a byte slice signature
-*/
-type Claim struct{}
-
-/*
-Consists of a version number,
-an array of Claims,
-and an array of Yields
-*/
-type Contract struct{}
-
 // Recipient will be SHA-256 hashed
 func MakeYield(recipient ecdsa.PublicKey, value uint64) Yield {
 	return Yield{}
 }
 
-func InsertYield(y Yield, database string, blockHeight uint32, string contractHash, uint64 ) {
-	// Open database connection,
-	// Insert into table:
-	// height of the block the yield is located in
-	// hash of the contract the yield is located in
-	// 
-}
-
-// Must remove UY's from database
-// Checks database for a yield
-func MakeClaim(database string, value uint64) (Claim, error) {
-	return Claim{}, errors.New("Incomplete function")
-}
-
-// Signs each Claim
-func (c *Contract) SignContract(ecdsa.PrivateKey) error {
-	return errors.New("Incomplete function")
-}
-
-// Wrapper function that builds the whole contract
-func MakeContract(version uint32, database string, sender ecdsa.PrivateKey, recipient ecdsa.PublicKey, value uint64) (Contract, error) {
-	// Assign version
-	// Get list of claims
-	/*
-		Should have a SumSoFar variable
-		While the SumSoFar is less than the value, keep
-		Making claims
-		If you run out of claims to make before
-		SumSoFar >= value, return an error
-		and restore all the removed yields in the database
-		If SumSoFar manages to exceed value,
-		Take the difference and make a Yield that goes to yourself.
-		That will be the change you get back
-	*/
-	// Get list of yields
-	/*
-		At minimum one yield for the recipient passed in as parameter
-		Possible change you get back
-	*/
-	return Contract{}, errors.New("Incomplete function")
-}
-
-func (c *Contract) Serialize() []byte {
-	return []byte{}
-}
+// func InsertYield(y Yield, database string, blockHeight uint32, string contractHash, uint64 ) {
+// 	// Open database connection,
+// 	// Insert into table:
+// 	// height of the block the yield is located in
+// 	// hash of the contract the yield is located in
+// 	//
+// }
 
 func (y *Yield) Serialize() []byte {
 	return []byte{}
 }
 
-func (k *Claim) Serialize() []byte {
-	return []byte{}
-}
-
-func DeserializeContract(b []byte) Contract {
-	return Contract{}
-}
-func DeserializeClaim(b []byte) Claim {
-	return Claim{}
-}
 func DeserializeYield(b []byte) Yield {
 	return Yield{}
 }
--- producer/src/contract/contract_test.go
+++ producer/src/contract/contract_test.go
@@ -6,11 +6,9 @@ import (
 	"crypto/elliptic"
 	"crypto/rand"
 	"database/sql"
-	"os"
 	"testing"
 
 	block "../block"
-	"github.com/google/go-cmp/cmp"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -31,17 +29,6 @@ func TestMakeYield(t *testing.T) {
 	}
 }
 
-// Make Claim Cases:
-/*
-Makes claim appropriately with one unclaimed-yield
-Does not make claim when there are no yields
-A priority test - not yet available
-Priority test would either be block height,
-absolute value difference between desired value and available yield values
-smallest values first
-largest values first
-*/
-
 func setUpDB(database string) {
 	conn, _ := sql.Open("sqlite3", database)
 	statement, _ := conn.Prepare(
@@ -53,111 +40,4 @@ func setUpDB(database string) {
 		value INTEGER)`)
 	statement.Exec()
 	conn.Close()
-}
-
-func TestMakeClaimSingle(t *testing.T) {
-	// Create table
-	setUpDB("testDB")
-	// Single element in table
-	contractHash := block.HashSHA256([]byte("previous contract"))
-	testPrivKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	testPubKey := testPrivKey.PublicKey
-	pubKeyBytes := testPubKey.X.Bytes()
-	pubKeyBytes = append(pubKeyBytes, testPubKey.Y.Bytes()...)
-	pubKeyStr := string(pubKeyBytes)
-	expected := Claim{
-		PreviousContractHash: contractHash,
-		YieldIndex:           0,
-		Holder:               testPubKey,
-	}
-	statement, _ = conn.Prepare(
-		`INSERT INTO unclaimed_yields(
-			block_height,
-			contract_hash, 
-			index, 
-			holder, 
-			value) 
-			values(?,?,?)`)
-	statement.Exec(35, contractHash, 0, pubKeyStr, 250)
-	conn.Close()
-	actual, err := MakeClaim(testDB, 250)
-	if err != nil {
-		t.Errorf("Failed to make claim on valid available yield")
-	}
-	if !(cmp.Equal(expected, actual)) {
-		t.Errorf("Claims do not match")
-		conn.Close()
-		os.Remove(testDB)
-	}
-	conn.Close()
-	os.Remove(testDB)
-}
-
-func TestMakeClaimEmpty(t *testing.T) {
-	// Create table
-	testDB := "testDatabase.db"
-	conn, _ := sql.Open("sqlite3", testDB)
-	statement, _ := conn.Prepare(
-		`CREATE TABLE IF NOT EXiSTS unclaimed_yields( 
-		block_height INTEGER,
-		contract_hash TEXT, 
-		index INTEGER, 
-		holder TEXT, 
-		value INTEGER)`)
-	statement.Exec()
-	conn.Close()
-	// No elements in table
-	// Claim should be empty
-	expected := Claim{}
-	actual, err := MakeClaim(testDB, 250)
-	if err == nil {
-		t.Errorf("Made claim on unavailable unclaimed-yield")
-	}
-	if !(cmp.Equal(expected, actual)) {
-		t.Errorf("Claims do not match")
-		conn.Close()
-		os.Remove(testDB)
-	}
-	conn.Close()
-	os.Remove(testDB)
-}
-
-func TestMakeContract(t *testing.T) {
-	// Create table
-	testDB := "testDatabase.db"
-	conn, _ := sql.Open("sqlite3", testDB)
-	statement, _ := conn.Prepare(
-		`CREATE TABLE IF NOT EXiSTS unclaimed_yields( 
-		block_height INTEGER,
-		contract_hash TEXT, 
-		index INTEGER, 
-		holder TEXT, 
-		value INTEGER)`)
-	statement.Exec()
-	// Insert Yields
-	// Exact amount
-	// Broke
-	// Change back
-}
-
-func TestSerializeContract(t *testing.T) {
-	testPrivKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	testPubKey := testPrivKey.PublicKey
-	testDB := "testDatabase.db"
-	conn, _ := sql.Open("sqlite3", testDB)
-	statement, _ := conn.Prepare(
-		`CREATE TABLE IF NOT EXiSTS unclaimed_yields( 
-		block_height INTEGER,
-		contract_hash TEXT, 
-		index INTEGER, 
-		holder TEXT, 
-		value INTEGER)`)
-	statement.Exec()
-	conn.Close()
-	c, err := MakeContract(1, testDB, testPrivKey, testPubKey, 50000)
-	serialized := c.Serialize()
-	deserialized := DeserializeContract(serialized)
-	if !cmp.Equal(c, deserialized) {
-		t.Errorf("Contracts don't match")
-	}
 }
