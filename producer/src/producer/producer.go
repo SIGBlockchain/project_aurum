7d6bf589464bc6f226d1dbdf2de0034560feacf0
karol <kstola2@uic.edu>
2019-04-01 12:00:46 -0500

    Update producer.go

--- producer/src/producer/producer.go
+++ producer/src/producer/producer.go
@@ -37,21 +37,6 @@ func (bp *BlockProducer) AcceptConnections() {
 	}
 }
 
-// Handles incoming Connections
-// Currently this is simply echoing messages back
-// In the future this will need to support messages of size > 1024
-// This can be done by reading in fragments
-
-/*
-Two types of messages:
-Balance:
-Given a public key, query the database for the current account balance
-** successful key find -> 2 fields, true and value
-** failed key find -> 1 field, false + greylist
-Sent a Contract:
-Validate contract, add to contract pool
-*/
-
 /*
 Should contain version, payload type, payload size
 */
@@ -63,41 +48,46 @@ Payloads should correspond to message type
 */
 type Message struct{}
 
-/*
-Should first check if messag is prefixed with keybytes
-Rejects messages without keybytes and add IP to to greylist
-*/
+// Handles incoming Connections
+// Currently this is simply echoing messages back
+// In the future this will need to support messages of size > 1024
+// This can be done by reading in fragments
 func (bp *BlockProducer) Handle(conn net.Conn) {
 	buf := make([]byte, 1024)
 	_, err := conn.Read(buf)
 	if err != nil {
 		return
 	}
+	/*
+		Check if keybytes are present.
+		If they aren't, close the connection, add IP to greylist
+
+		Check type of message (Balance or Contract)
+
+		If Balance, query the database with the public key
+		If no public key exists, send message with `invalid public key`
+		Otherwise send balance back
+
+		If Contract, validate contract first
+		If validation fails, send message with `invalid contract`
+		Otherwise, add contract to contract pool and send verification message
+	*/
 	conn.Write(buf)
 	conn.Close()
 }
 
 // The main work loop
 // Handles communication, block production, and ledger maintenance
-
-/*
-cases:
-new connection -> should call Handle but not as a separate thread
-sig int -> should gracefully exit
-default -> checks to see if it's time to update the ledger
-** if it is time, then create a block and add it
-*** it is time if (time now - time of last block) = some time interval
-*** Creating a block should involve extracting from the contract pool
-*** May require knowing what the last block is
-*/
 func (bp *BlockProducer) WorkLoop(logger *log.Logger) {
 	// Creates signal
 	signalCh := make(chan os.Signal, 1)
 	signal.Notify(signalCh, syscall.SIGINT, syscall.SIGTERM)
+	// TODO: Extract last block blockhash, timestamp
+	// Initialize contract pool
 	for {
 		select {
 		case conn := <-bp.NewConnection:
-			go bp.Handle(conn)
+			go bp.Handle(conn) // TODO: remove `go`
 		// If an interrupt signal is encountered, exit
 		case <-signalCh:
 			// If loop is exited properly, interrupt signal had been recieved
@@ -105,7 +95,14 @@ func (bp *BlockProducer) WorkLoop(logger *log.Logger) {
 			logger.Println("Interrupt signal encountered, program terminating.")
 			return
 		default:
-			// Do other stuff
+			/*
+				TODO:
+				Check to see if it's time to make a block
+				Block interval = (timeNow - timeSinceLastBlock)
+				If it is time, make the block from the contract pool
+				(Merkle Root, add block)
+				Reset last block metadata
+			*/
 		}
 	}
 }
