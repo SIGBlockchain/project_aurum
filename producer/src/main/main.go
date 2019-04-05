5d8864b3a73e34317a1da405962d211ddad3bbb8
karol <kstola2@uic.edu>
2019-04-05 16:49:23 -0500

    Added environment variable parsing

--- producer/src/main/main.go
+++ producer/src/main/main.go
@@ -1,36 +1,68 @@
 package main
 
 import (
+	"bytes"
 	"log"
 	"net"
 	"os"
 
 	producer "github.com/SIGBlockchain/project_aurum/producer/src/producer"
+	"github.com/pborman/getopt"
 )
 
 func main() {
+	// Command line parsing
+	help := getopt.Bool('?', "Display Valid Flags")
+	debug := getopt.BoolLong("debug", 'd', "Enable Debug Mode")
+	logFile := getopt.StringLong("log", 'l', "", "Log File Location")
+	port := getopt.StringLong("port", 'p', "13131", "Port Number")
+	getopt.CommandLine.Lookup('l').SetOptional()
+	getopt.Parse()
+
+	// If the help flag is on, print usage to os.Stdout
+	if *help == true {
+		getopt.Usage()
+		os.Exit(0)
+	}
+
 	// Initialize logger
 	logger := log.New(os.Stdout, "LOG: ", log.Ldate|log.Lmicroseconds|log.Lshortfile)
+	if *debug == false {
+		var buffer bytes.Buffer
+		logger = log.New(&buffer, "LOG: ", log.Ldate|log.Lmicroseconds|log.Lshortfile)
+	}
+
+	if getopt.CommandLine.Lookup('l').Count() > 0 {
+		filepath := os.Getenv("GOPATH") + "/src/github.com/SIGBlockchain/project_aurum/producer/logs"
+		os.Mkdir(filepath, 0777)
+		// If no filename is given, logs.txt
+		if *logFile == "" {
+			filepath += "/logs.txt"
+			// Otherwise the custom filename is used
+		} else {
+			filepath += "/" + *logFile
+		}
+		f, err := os.OpenFile(filepath, os.O_RDWR|os.O_CREATE, 0666)
+		defer f.Close()
+
+		// If there is any error, do not set the logger. Log an error messgae
+		if err != nil {
+			logger.Fatalln(err)
+		} else {
+			logger = log.New(f, "LOG: ", log.Ldate|log.Lmicroseconds|log.Lshortfile)
+		}
+	}
 
 	// Check to see if there is an internet connection
 	err := producer.CheckConnectivity()
 	if err != nil {
 		logger.Fatalln("Connectivity check failed.")
-
 	}
 	logger.Println("Connection check passed.")
 
-	// Default port
-	port := "13131"
-
-	// Grabs port
-	if len(os.Args) > 1 {
-		port = os.Args[1]
-	}
-
 	// Spin up server
 	// NOTE: If this doesn't work, try deleting `localhost`
-	ln, err := net.Listen("tcp", "localhost:"+port)
+	ln, err := net.Listen("tcp", "localhost:"+*port)
 	if err != nil {
 		logger.Fatalln("Failed to start server.")
 	}
@@ -43,7 +75,7 @@ func main() {
 	}
 
 	// Start listening for connections
-	logger.Printf("Server listening on port %s.\n", port)
+	logger.Printf("Server listening on port %s.\n", *port)
 	go bp.AcceptConnections()
 
 	// Main loop
