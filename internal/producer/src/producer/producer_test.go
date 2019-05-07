8643344f84ff971f949a5ffb8bc5da75b491b626
karol <kstola2@uic.edu>
2019-05-07 00:38:30 -0500

    added genesis and airdrop tests, also reinstated nonce for contracts

--- internal/producer/src/producer/producer_test.go
+++ internal/producer/src/producer/producer_test.go
@@ -2,9 +2,21 @@ package producer
 
 import (
 	"bytes"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"encoding/binary"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
+	"reflect"
 	"testing"
+	"time"
+
+	"github.com/SIGBlockchain/project_aurum/internal/producer/src/accounts"
+	"github.com/SIGBlockchain/project_aurum/internal/producer/src/block"
+	"github.com/SIGBlockchain/project_aurum/pkg/keys"
 )
 
 // Test will fail in airplane mode, or just remove wireless connection.
@@ -66,4 +78,270 @@ func TestHandler(t *testing.T) {
 	}
 	conn.Close()
 	ln.Close()
+}
+
+func TestData_Serialize(t *testing.T) {
+	senderPrivateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	spkh := block.HashSHA256(keys.EncodePublicKey(&senderPrivateKey.PublicKey))
+	initialContract, _ := accounts.MakeContract(1, nil, spkh, 1000, 0)
+	initialContract.StateNonce = 0
+	tests := []struct {
+		name string
+		d    *Data
+	}{
+		{
+			d: &Data{
+				Hdr: DataHeader{
+					Version: 1,
+					Type:    0,
+				},
+				Bdy: initialContract,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.d.Serialize()
+			if err != nil {
+				t.Errorf(err.Error())
+			}
+			defer func() {
+				if r := recover(); r != nil {
+					t.Errorf("panicked, check indexing")
+				}
+			}()
+			serializedInitialContract, err := tt.d.Bdy.Serialize()
+			if err != nil {
+				t.Errorf(err.Error())
+			}
+			serializedVersion := make([]byte, 2)
+			binary.LittleEndian.PutUint16(serializedVersion, 1)
+			serializedType := make([]byte, 2)
+			binary.LittleEndian.PutUint16(serializedType, 0)
+			if !bytes.Equal(got[:2], serializedVersion) {
+				t.Errorf(fmt.Sprintf("Data header version serialization does not match. Wanted: %v, got: %v", serializedVersion, got[:2]))
+			}
+			if !bytes.Equal(got[2:4], serializedType) {
+				t.Errorf(fmt.Sprintf("Data header type serialization does not match. Wanted: %v, got: %v", serializedVersion, got[2:4]))
+			}
+			if !bytes.Equal(got[4:], serializedInitialContract) {
+				t.Errorf(fmt.Sprintf("Data header body serialization does not match. Wanted: %v, got: %v", serializedVersion, got[4:]))
+			}
+		})
+	}
+}
+
+func TestData_Deserialize(t *testing.T) {
+	senderPrivateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	spkh := block.HashSHA256(keys.EncodePublicKey(&senderPrivateKey.PublicKey))
+	initialContract, _ := accounts.MakeContract(1, nil, spkh, 1000, 0)
+	initialContract.StateNonce = 0
+	someData := &Data{
+		Hdr: DataHeader{
+			Version: 1,
+			Type:    0,
+		},
+		Bdy: initialContract,
+	}
+	serializedInitialContract, _ := initialContract.Serialize()
+	type args struct {
+		serializedData []byte
+	}
+	tests := []struct {
+		name    string
+		d       *Data
+		args    args
+		wantErr bool
+	}{
+		{
+			d: &Data{},
+			args: args{
+				serializedData: serializedInitialContract,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.d.Deserialize(tt.args.serializedData); (err != nil) != tt.wantErr {
+				t.Errorf("Data.Deserialize() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !reflect.DeepEqual(tt.d, someData) {
+				t.Errorf("Deserialized Data struct failed to match")
+			}
+		})
+	}
+}
+
+func TestCreateBlock(t *testing.T) {
+	var datum []Data
+	for i := 0; i < 50; i++ {
+		someKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+		someKeyPKHash := block.HashSHA256(keys.EncodePublicKey(&someKey.PublicKey))
+		someAirdropContract, _ := accounts.MakeContract(1, nil, someKeyPKHash, 1000, 0)
+		someAirdropContract.StateNonce = 0
+		someDataHdr := DataHeader{
+			Version: 1,
+			Type:    0,
+		}
+		someData := Data{
+			Hdr: someDataHdr,
+			Bdy: someAirdropContract,
+		}
+		datum = append(datum, someData)
+	}
+	var serializedDatum [][]byte
+	for i := range datum {
+		serialData, _ := datum[i].Serialize()
+		serializedDatum = append(serializedDatum, serialData)
+	}
+	type args struct {
+		version      uint16
+		height       uint64
+		previousHash []byte
+		data         []Data
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    block.Block
+		wantErr bool
+	}{
+		{
+			args: args{
+				version:      1,
+				height:       0,
+				previousHash: make([]byte, 32),
+				data:         datum,
+			},
+			wantErr: false,
+			want: block.Block{
+				Version:        1,
+				Height:         0,
+				Timestamp:      time.Now().UnixNano(),
+				PreviousHash:   make([]byte, 32),
+				MerkleRootHash: block.GetMerkleRootHash(serializedDatum),
+				Data:           serializedDatum,
+				DataLen:        50,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := CreateBlock(tt.args.version, tt.args.height, tt.args.previousHash, tt.args.data)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CreateBlock() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CreateBlock() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBringOnTheGenesis(t *testing.T) {
+	var pkhashes [][]byte
+	var datum []Data
+	for i := 0; i < 100; i++ {
+		someKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+		someKeyPKHash := block.HashSHA256(keys.EncodePublicKey(&someKey.PublicKey))
+		pkhashes = append(pkhashes, someKeyPKHash)
+		someAirdropContract, _ := accounts.MakeContract(1, nil, someKeyPKHash, 1000, 0)
+		someAirdropContract.StateNonce = 0
+		someDataHdr := DataHeader{
+			Version: 1,
+			Type:    0,
+		}
+		someData := Data{
+			Hdr: someDataHdr,
+			Bdy: someAirdropContract,
+		}
+		datum = append(datum, someData)
+	}
+	genny, _ := CreateBlock(1, 0, make([]byte, 32), datum)
+	type args struct {
+		genesisPublicKeyHashes [][]byte
+		initialAurumSupply     uint64
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    block.Block
+		wantErr bool
+	}{
+		{
+			args: args{
+				genesisPublicKeyHashes: pkhashes,
+				initialAurumSupply:     1000,
+			},
+			want:    genny,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := BringOnTheGenesis(tt.args.genesisPublicKeyHashes, tt.args.initialAurumSupply)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BringOnTheGenesis() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("BringOnTheGenesis() = %v, want %v", got, tt.want)
+			}
+			for i := range got.Data {
+				deserializedData := Data{}
+				err := deserializedData.Deserialize(got.Data[i])
+				if err != nil {
+					t.Errorf("failed to deserialize data")
+				}
+				deserializedContract := &accounts.Contract{}
+				serializedDataBdy, _ := deserializedData.Bdy.Serialize()
+				deserializedContract.Deserialize(serializedDataBdy)
+				if deserializedContract.Value != 10 {
+					t.Errorf("failed to distribute aurum properly")
+				}
+			}
+		})
+	}
+}
+
+func TestAirdrop(t *testing.T) {
+	var pkhashes [][]byte
+	for i := 0; i < 100; i++ {
+		someKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+		someKeyPKHash := block.HashSHA256(keys.EncodePublicKey(&someKey.PublicKey))
+		pkhashes = append(pkhashes, someKeyPKHash)
+	}
+	genny, _ := BringOnTheGenesis(pkhashes, 1000)
+	type args struct {
+		blockchain   string
+		genesisBlock block.Block
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			args: args{
+				blockchain:   "blockchain.dat",
+				genesisBlock: genny,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := Airdrop(tt.args.blockchain, tt.args.genesisBlock); (err != nil) != tt.wantErr {
+				t.Errorf("Airdrop() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			fileGenny, err := ioutil.ReadFile(tt.args.blockchain)
+			if err != nil {
+				t.Errorf("Failed to open file" + err.Error())
+			}
+			serializedGenny := genny.Serialize()
+			if !bytes.Equal(fileGenny, serializedGenny) {
+				t.Errorf("Genesis block does not match file block")
+			}
+		})
+	}
 }
