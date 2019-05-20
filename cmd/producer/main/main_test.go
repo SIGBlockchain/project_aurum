989522a7fb7a0f93371d193e5d5471d0d79e0d86
karol <kstola2@uic.edu>
2019-05-20 18:19:37 -0500

    Update main_test.go

--- cmd/producer/main/main_test.go
+++ cmd/producer/main/main_test.go
@@ -42,9 +42,37 @@ func TestProducerGenesisFlag(t *testing.T) {
 
 func TestLoop(t *testing.T) {
 	producer.GenerateGenesisHashFile(25)
-	cmd := exec.Command("go", "run", "-d", "-g", "--supply=100")
-	err := cmd.Start()
-	if err != nil {
-		t.Errorf("failed to run main command: %s", err.Error())
+	defer func() { // Function is dangerous, consider only running with flag
+		if removeFiles {
+			if err := os.Remove("genesis_hashes.txt"); err != nil {
+				t.Errorf("failed to remove genesis hashes")
+			}
+			if err := os.Remove("blockchain.dat"); err != nil {
+				t.Errorf("failed to remove blockchain.dat")
+			}
+			if err := os.Remove("metadata.tab"); err != nil {
+				t.Errorf("failed to remove metadatata.tab")
+			}
+		}
+	}()
+	cmd := exec.Command("go", "run", "main.go", "-d", "-g", "--supply=100")
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+	if err := cmd.Start(); err != nil {
+		t.Errorf("failed to run main command because: %s", err.Error())
 	}
+	if err := cmd.Wait(); err != nil {
+		t.Errorf("main call returned with: %s.", err.Error())
+		t.Logf("Stderr: %s", string(stderr.Bytes()))
+	}
+	cmd = exec.Command("go", "run", "main.go", "-d", "--interval=5000ms")
+	cmd.Stderr = &stderr
+	if err := cmd.Start(); err != nil {
+		t.Errorf("failed to run main command because: %s", err.Error())
+	}
+	if err := cmd.Wait(); err != nil {
+		t.Errorf("main call returned with: %s.", err.Error())
+		t.Logf("Stderr: %s", string(stderr.Bytes()))
+	}
+
 }
