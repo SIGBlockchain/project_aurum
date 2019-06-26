9c7e7bacbb3c823a63f013da28f8cd33e8be3e84
karol <kstola2@uic.edu>
2019-06-26 00:07:55 -0500

    added new endpoint, request and test

--- internal/requests/requests.go
+++ internal/requests/requests.go
@@ -1,12 +1,30 @@
 package requests
 
 import (
+	"bytes"
+	"encoding/hex"
+	"encoding/json"
 	"errors"
 	"net/http"
+
+	"github.com/SIGBlockchain/project_aurum/pkg/keys"
+
+	"github.com/SIGBlockchain/project_aurum/internal/endpoints"
+	"github.com/SIGBlockchain/project_aurum/internal/producer/src/accounts"
 )
 
+type JSONContract struct {
+	Version                uint16
+	SenderPublicKey        string
+	SignatureLength        uint8
+	Signature              string
+	RecipientWalletAddress string
+	Value                  uint64
+	StateNonce             uint64
+}
+
 func NewAccountInfoRequest(host string, walletAddress string) (*http.Request, error) {
-	req, err := http.NewRequest(http.MethodGet, host+"/accountinfo", nil)
+	req, err := http.NewRequest(http.MethodGet, host+endpoints.AccountInfo, nil)
 	if err != nil {
 		return nil, errors.New("Failed to make new request:\n" + err.Error())
 	}
@@ -14,4 +32,28 @@ func NewAccountInfoRequest(host string, walletAddress string) (*http.Request, er
 	values.Add("w", walletAddress)
 	req.URL.RawQuery = values.Encode()
 	return req, nil
+}
+
+func NewContractRequest(host string, newContract accounts.Contract) (*http.Request, error) {
+	// TODO: accounts.Contract to JSON
+	var newJSONContract = JSONContract{
+		Version:                newContract.Version,
+		SenderPublicKey:        hex.EncodeToString(keys.EncodePublicKey(newContract.SenderPubKey)),
+		SignatureLength:        newContract.SigLen,
+		Signature:              hex.EncodeToString(newContract.Signature),
+		RecipientWalletAddress: hex.EncodeToString(newContract.RecipPubKeyHash),
+		Value:      newContract.Value,
+		StateNonce: newContract.StateNonce,
+	}
+	marshalledContract, err := json.Marshal(newJSONContract)
+	if err != nil {
+		return nil, errors.New("Failed to marshall contract: " + err.Error())
+	}
+	req, err := http.NewRequest(http.MethodPost, host+endpoints.Contract, bytes.NewBuffer(marshalledContract))
+	if err != nil {
+		return nil, errors.New("Failed to create request: " + err.Error())
+	}
+	req.Header.Set("Content-Type", "application/json")
+	return req, nil
+
 }
