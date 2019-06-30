953b03e4c3756e6e9d3676e149e9143bbcc698ce
karol <kstola2@uic.edu>
2019-06-30 09:47:38 -0500

    Added config test

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -1,1 +1,34 @@
 package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/SIGBlockchain/project_aurum/internal/constants"
+)
+
+func TestLoadConfigurationFile(t *testing.T) {
+	cfg := Config{1, 20, "5000", "40s", false}
+	marshalledCfg, err := json.Marshal(cfg)
+	if err != nil {
+		t.Errorf("failed to marshall configuration struct: %v", err)
+	}
+	if err := ioutil.WriteFile(constants.ConfigurationFile, marshalledCfg, os.ModePerm); err != nil {
+		t.Errorf("failed to write to file: %v", err)
+	}
+	defer func() {
+		if err := os.Remove(constants.ConfigurationFile); err != nil {
+			t.Errorf("failed to remove config file: %v", err)
+		}
+	}()
+	fileCfg, err := LoadConfiguration()
+	if err != nil {
+		t.Errorf("failed to load configuration file: %v", err)
+	}
+	if !reflect.DeepEqual(cfg, *fileCfg) {
+		t.Errorf("structs to not match: got %+v want %+v", *fileCfg, cfg)
+	}
+}
