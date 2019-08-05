e3ff6e4d464b84317af8415553320cc4f600603b
kstola2 <kstola2@uic.edu>
2019-08-04 21:46:27 -0500

    Revert "Add error returns to keys functions.#228"
    This reverts commit cd4767408f8d5460ace506ba612c269633dd8e51.

--- internal/publickey/publickey_test.go
+++ internal/publickey/publickey_test.go
@@ -4,59 +4,26 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
-	"crypto/x509"
-	"encoding/pem"
 	"reflect"
 	"testing"
 )
 
-// test that public keys can be encoded properly
+// Full test for encoding/decoding public keys
 func TestEncoding(t *testing.T) {
 	private, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	public := private.PublicKey
-	x509EncodedPub, err := x509.MarshalPKIXPublicKey(nil)
-	// test that Encoding returns an error for bad input
-	if err == nil {
-		t.Errorf("Expected err to not be nil, but it was...")
+	encoded := Encode(&public)
+	decoded := Decode(encoded)
+
+	if !reflect.DeepEqual(public, *decoded) {
+		t.Errorf("Keys do not match")
 	}
-	encoded, err := Encode(&public)
-	// test that Encoding does not receive an error for valid input
-	if err != nil {
-		t.Errorf("Received an error for valid input")
-	}
-	x509EncodedPub, _ = x509.MarshalPKIXPublicKey(&public)
-	x509EncodedPub = pem.EncodeToMemory(&pem.Block{Type: "PUBLIC KEY", Bytes: x509EncodedPub})
-	// test that Encoding results match
-	if !reflect.DeepEqual(x509EncodedPub, encoded) {
-		t.Errorf("Encoding does not match")
+	reEncoded := Encode(decoded)
+	if !reflect.DeepEqual(reEncoded, encoded) {
+		t.Errorf("Encoded keys do not match")
 	}
 }
 
-// test that public keys can be decoded properly
-func TestDecoding(t *testing.T) {
-	private, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	public := private.PublicKey
-	encoded, _ := Encode(&public)
-	decoded, err := Decode(nil)
-	// test that Decoding returns an error
-	if err == nil {
-		t.Errorf("Expected err to not be nil, but it was...")
-	}
-	decoded, err = Decode(encoded)
-	// test that Decoding does not return an error for valid input
-	if err != nil {
-		t.Errorf("Expected err to not be nil, but it was...")
-	}
-	localDecoded, _ := pem.Decode(encoded)
-	x509EncodedPub := localDecoded.Bytes
-	genericPublicKey, _ := x509.ParsePKIXPublicKey(x509EncodedPub)
-	// test that decodings match
-	if !reflect.DeepEqual(genericPublicKey, decoded) {
-		t.Errorf("Keys do not match after decode")
-	}
-}
-
-// tests for Equals function for public keys
 func TestEquals(t *testing.T) {
 	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	if err != nil {
