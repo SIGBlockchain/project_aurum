e553969d70405414244d6b424101a84c0e4e7550
kstola <kstola2@uic.edu>
2019-08-20 21:57:04 -0500

    Finished handling  account info request exploit

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -17,25 +17,11 @@ import (
 )
 
 // Handler for incoming account info queries
-func HandleAccountInfoRequest(dbConn *sql.DB) func(w http.ResponseWriter, r *http.Request) {
+func HandleAccountInfoRequest(dbConn *sql.DB, pMap pendingpool.PendingMap, pendingLock *sync.Mutex) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		var walletAddress = r.URL.Query().Get("w") // assume this is hex-encoded
-		// Query the database
-		// TODO: will most likely need a lock on this dbConnection everywhere
-		row, err := dbConn.Query(sqlstatements.GET_EVERYTHING_FROM_ACCOUNT_BALANCE_BY_WALLETADDRESS, walletAddress)
+		var reqestingWalletAddress = r.URL.Query().Get("w") // assume this is hex-encoded
 
-		if err != nil {
-			w.WriteHeader(http.StatusServiceUnavailable)
-			io.WriteString(w, err.Error())
-			return
-		}
-		defer row.Close()
-		// If there is no row with the corresponding wallet address, return not found
-		if !row.Next() {
-			w.WriteHeader(http.StatusNotFound)
-			return
-		}
 		// TODO: Remake this struct
 		type AccountInfo struct {
 			WalletAddress string
@@ -43,13 +29,41 @@ func HandleAccountInfoRequest(dbConn *sql.DB) func(w http.ResponseWriter, r *htt
 			StateNonce    uint64
 		}
 		var accInfo AccountInfo
-		// Fill the Account info struct
-		err = row.Scan(&accInfo.WalletAddress, &accInfo.Balance, &accInfo.StateNonce)
-		if err != nil {
-			w.WriteHeader(http.StatusNoContent)
-			io.WriteString(w, err.Error())
-			return
+		var marshalledStruct []byte
+
+		pendingLock.Lock()
+		pendingData, ok := pMap.Sender[reqestingWalletAddress]
+		pendingLock.Unlock()
+
+		if ok {
+			accInfo = AccountInfo{reqestingWalletAddress, pendingData.PendingBal, pendingData.PendingNonce}
+		} else {
+
+			// Query the database
+			// TODO: will most likely need a lock on this dbConnection everywhere
+			row, err := dbConn.Query(sqlstatements.GET_EVERYTHING_FROM_ACCOUNT_BALANCE_BY_WALLETADDRESS, reqestingWalletAddress)
+
+			if err != nil {
+				w.WriteHeader(http.StatusServiceUnavailable)
+				io.WriteString(w, err.Error())
+				return
+			}
+			defer row.Close()
+			// If there is no row with the corresponding wallet address, return not found
+			if !row.Next() {
+				w.WriteHeader(http.StatusNotFound)
+				return
+			}
+
+			// Fill the Account info struct
+			err = row.Scan(&accInfo.WalletAddress, &accInfo.Balance, &accInfo.StateNonce)
+			if err != nil {
+				w.WriteHeader(http.StatusNoContent)
+				io.WriteString(w, err.Error())
+				return
+			}
 		}
+
 		// Marshall the struct into the response body
 		marshalledStruct, err := json.Marshal(accInfo)
 		if err != nil {
