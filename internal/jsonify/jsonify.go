a9628361d9729a3599d593accdeafd5587fa1d91
ZBASH <zlabas2@uic.edu>
2019-08-26 14:09:31 -0500

    Updated package to use interface instead of struct

--- internal/jsonify/jsonify.go
+++ internal/jsonify/jsonify.go
@@ -7,26 +7,16 @@ import (
 	"os"
 )
 
-//TODO - improve this description: stores file information as structured json
-type Config struct {
-	Version                 uint16
-	InitialAurumSupply      uint64
-	Port                    string
-	BlockProductionInterval string
-	Localhost               bool
-}
-
 // is the assumption that the file is already open?
-// if the function is being returned and the interface needs to be defined locally, why pass an interface?
 // ok to return an error?
-// convert file to interface 
-func toJSON(file *os.File, config interface{}) (*Config, error) {
+// convert file to interface
+func toInterface(file *os.File, iFace interface{}) (*interface{}, error) {
 	cfgData, err := ioutil.ReadAll(file)
 	if err != nil {
 		return nil, errors.New("Failed to read configuration file : " + err.Error())
 	}
 
-	cfg := Config{}
+	cfg := iFace
 	if err := json.Unmarshal(cfgData, &cfg); err != nil {
 		return nil, errors.New("Failed to unmarshall configuration data : " + err.Error())
 	}
