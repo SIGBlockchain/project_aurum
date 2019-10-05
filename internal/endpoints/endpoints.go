100b47d39597101187c86dc551553ff00050c679
harshalpatel165 <55267253+harshalpatel165@users.noreply.github.com>
2019-10-05 14:01:03 -0500

    Additionalconstants (#355)
    * added constant that shows the endpoint for incoming blocks
    
    * added endpoints for incoming block by height and hash
    
    * added endpoints for incoming block by height and hash
    
    * test for additional constants

--- internal/endpoints/endpoints.go
+++ internal/endpoints/endpoints.go
@@ -1,8 +1,10 @@
 package endpoints
 
 const (
-	AccountInfo   = "/accountinfo"
-	Contract      = "/contract"
-	AddPeer       = "http://blockchain.acmapp.tech/peer/add"
-	IncomingBlock = "/block"
+	AccountInfo        = "/accountinfo"
+	Contract           = "/contract"
+	AddPeer            = "http://blockchain.acmapp.tech/peer/add"
+	IncomingBlock      = "/block"
+	BlockQueryByHeight = "/block/height"
+	BlockQueryByHash   = "/block/hash"
 )
