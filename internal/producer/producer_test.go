83a5b0ffdf50bf5ad3ca68ffe31af7436162c6af
Kyle Trufin <kyletrufin@gmail.com>
2019-11-11 14:08:35 -0600

    deleted unused imports

--- internal/producer/producer_test.go
+++ internal/producer/producer_test.go
@@ -13,16 +13,12 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/SIGBlockchain/project_aurum/internal/accountinfo"
-	"github.com/SIGBlockchain/project_aurum/internal/accountstable"
 	"github.com/SIGBlockchain/project_aurum/internal/blockchain"
 	"github.com/SIGBlockchain/project_aurum/internal/constants"
 	"github.com/SIGBlockchain/project_aurum/internal/contracts"
 	"github.com/SIGBlockchain/project_aurum/internal/genesis"
 	"github.com/SIGBlockchain/project_aurum/internal/hashing"
 	"github.com/SIGBlockchain/project_aurum/internal/publickey"
-	"github.com/SIGBlockchain/project_aurum/internal/sqlstatements"
-	"github.com/SIGBlockchain/project_aurum/internal/wallet"
 )
 
 var removeFiles = true
