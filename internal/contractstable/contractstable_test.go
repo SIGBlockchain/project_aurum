12893e0c28143329fbbb3f212a57708612ebba9a
Vineet77 <vineetrpatel@gmail.com>
2020-03-05 16:14:55 -0600

    Fixed test by using recipient hash.

--- internal/contractstable/contractstable_test.go
+++ internal/contractstable/contractstable_test.go
@@ -5,6 +5,7 @@ import (
 	"crypto/elliptic"
 	"crypto/rand"
 	"database/sql"
+	"github.com/SIGBlockchain/project_aurum/internal/hashing"
 	"os"
 	"testing"
 
@@ -40,7 +41,8 @@ func TestInsertContractIntoContractsTable(t *testing.T) {
 	senderPrivateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	recipientPrivateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	encodedRecipientKey, _ := publickey.Encode(&recipientPrivateKey.PublicKey)
-	contract, _ := contracts.New(1, senderPrivateKey, encodedRecipientKey, 1, 1)
+	recipientPubKeyHash := hashing.New(encodedRecipientKey)
+	contract, _ := contracts.New(1, senderPrivateKey, recipientPubKeyHash, 1, 1)
 
 	// Act
 	err := InsertContractIntoContractsTable(contract)
@@ -60,12 +62,13 @@ func TestInsertContractsIntoContractsTable(t *testing.T) {
 	senderPrivateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	recipientPrivateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	encodedRecipientKey, _ := publickey.Encode(&recipientPrivateKey.PublicKey)
+	recipientPubKeyHash := hashing.New(encodedRecipientKey)
 	var testContracts [](*contracts.Contract)
 
-	contract1, _ := contracts.New(1, senderPrivateKey, encodedRecipientKey, 1, 1)
+	contract1, _ := contracts.New(1, senderPrivateKey, recipientPubKeyHash, 1, 1)
 	testContracts = append(testContracts, contract1)
 
-	contract2, _ := contracts.New(1, senderPrivateKey, encodedRecipientKey, 10, 2)
+	contract2, _ := contracts.New(1, senderPrivateKey, recipientPubKeyHash, 10, 2)
 	testContracts = append(testContracts, contract2)
 
 	// Act
@@ -86,35 +89,36 @@ func TestGetContractsFromContractsTable(t *testing.T) {
 	senderPrivateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	recipientPrivateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	encodedRecipientKey, _ := publickey.Encode(&recipientPrivateKey.PublicKey)
+	recipientPubKeyHash := hashing.New(encodedRecipientKey)
 	var testContracts [](*contracts.Contract)
 
-	contract1, _ := contracts.New(1, senderPrivateKey, encodedRecipientKey, 1, 1)
+	contract1, _ := contracts.New(1, senderPrivateKey, recipientPubKeyHash, 1, 1)
 	err := InsertContractIntoContractsTable(contract1)
 	testContracts = append(testContracts, contract1)
 
-	contract2, _ := contracts.New(1, senderPrivateKey, encodedRecipientKey, 10, 2)
+	contract2, _ := contracts.New(1, senderPrivateKey, recipientPubKeyHash, 10, 2)
 	err2 := InsertContractIntoContractsTable(contract2)
 	testContracts = append(testContracts, contract2)
 
 	if err != nil || err2 != nil {
-		t.Errorf("Failed to insert contract into contracts table: %s", err)
+		t.Errorf("Failed to insert contract into tableContracts table: %s", err)
 	}
 
 	// Act
-	contracts, err := GetContractsFromContractsTable(dbConn, &senderPrivateKey.PublicKey, 1)
+	tableContracts, err := GetContractsFromContractsTable(dbConn, &senderPrivateKey.PublicKey, 1)
 	if err != nil {
-		t.Errorf("Failed to get contracts from contracts table")
+		t.Errorf("Failed to get tableContracts from tableContracts table")
 	}
 
 	// Assert
-	if len(contracts) != len(testContracts) {
-		t.Errorf("Failed to get the correct number of contracts from contracts table")
+	if len(tableContracts) != len(testContracts) {
+		t.Errorf("Failed to get the correct number of tableContracts from tableContracts table")
 	}
 
-	for n := 0; n < len(contracts); n++ {
-		result := (*contracts[n]).Equals(*testContracts[n])
+	for n := 0; n < len(tableContracts); n++ {
+		result := (*tableContracts[n]).Equals(*testContracts[n])
 		if !result {
-			t.Errorf("Failed to get the expected contracts from contracts table")
+			t.Errorf("Failed to get the expected tableContracts from tableContracts table")
 		}
 	}
 }
