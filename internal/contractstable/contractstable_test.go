3c35b0d25b97a9edafd06ac7681b31283ada2f01
HarryL5004 <loklok8795@gmail.com>
2020-03-05 23:38:46 -0600

    Updated tests to include database queries. Minor changes to sql statements #442

--- internal/contractstable/contractstable_test.go
+++ internal/contractstable/contractstable_test.go
@@ -5,7 +5,9 @@ import (
 	"crypto/elliptic"
 	"crypto/rand"
 	"database/sql"
+	"encoding/hex"
 	"os"
+	"strconv"
 	"testing"
 
 	"github.com/SIGBlockchain/project_aurum/internal/hashing"
@@ -40,9 +42,12 @@ func TestInsertContractIntoContractsTable(t *testing.T) {
 	defer tearDown(dbConn, "testContracts.db")
 
 	senderPrivateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	encodedSenderPubkey, _ := publickey.Encode(&senderPrivateKey.PublicKey)
+
 	recipientPrivateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	encodedRecipientKey, _ := publickey.Encode(&recipientPrivateKey.PublicKey)
 	recipientPubKeyHash := hashing.New(encodedRecipientKey)
+
 	contract, _ := contracts.New(1, senderPrivateKey, recipientPubKeyHash, 1, 1)
 
 	// Act
@@ -53,6 +58,25 @@ func TestInsertContractIntoContractsTable(t *testing.T) {
 		t.Errorf("Failed to insert contract into contracts table: %s", err)
 	}
 
+	dbConn.Prepare(sqlstatements.GET_CONTRACT_BY_SENDER_PUBLIC_KEY_AND_NONCE)
+	row, err := dbConn.Query(hex.EncodeToString(hashing.New(encodedSenderPubkey)), strconv.FormatUint(contract.StateNonce, 10))
+	if err != nil {
+		t.Errorf("Failed to acquire rows from contracts table")
+	}
+	row.Next()
+	var result []byte
+	err = row.Scan(&result)
+	if err != nil {
+		t.Errorf("Failed to scan rows: %s", err)
+	}
+	var cResult *contracts.Contract
+	err = cResult.Deserialize(result)
+	if err != nil {
+		t.Errorf("Failed to deserialize contract")
+	}
+	if !cResult.Equals(*contract) {
+		t.Error("Result contract does not equal to contract.\nWant: " + cResult.ToString() + "\nGot: " + contract.ToString())
+	}
 }
 
 func TestInsertContractsIntoContractsTable(t *testing.T) {
@@ -61,25 +85,46 @@ func TestInsertContractsIntoContractsTable(t *testing.T) {
 	defer tearDown(dbConn, "testContracts.db")
 
 	senderPrivateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	encodedSenderPubkey, _ := publickey.Encode(&senderPrivateKey.PublicKey)
+
 	recipientPrivateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	encodedRecipientKey, _ := publickey.Encode(&recipientPrivateKey.PublicKey)
 	recipientPubKeyHash := hashing.New(encodedRecipientKey)
+
 	var testContracts [](*contracts.Contract)
-
 	contract1, _ := contracts.New(1, senderPrivateKey, recipientPubKeyHash, 1, 1)
-	testContracts = append(testContracts, contract1)
-
 	contract2, _ := contracts.New(1, senderPrivateKey, recipientPubKeyHash, 10, 2)
-	testContracts = append(testContracts, contract2)
+	testContracts = append(testContracts, contract1, contract2)
 
 	// Act
 	err := InsertContractsIntoContractsTable(testContracts)
 
 	// Assert
 	if err != nil {
-		t.Errorf("Failed to insert contracts into contracts table: %s", err)
+		t.Errorf("Failed to insert multiple contracts into contracts table: %s", err)
 	}
 
+	for _, contract := range testContracts {
+		dbConn.Prepare(sqlstatements.GET_CONTRACT_BY_SENDER_PUBLIC_KEY_AND_NONCE)
+		row, err := dbConn.Query(hex.EncodeToString(hashing.New(encodedSenderPubkey)), strconv.FormatUint(contract.StateNonce, 10))
+		if err != nil {
+			t.Errorf("Failed to acquire rows from contracts table")
+		}
+		row.Next()
+		var result []byte
+		err = row.Scan(&result)
+		if err != nil {
+			t.Errorf("Failed to scan rows: %s", err)
+		}
+		var cResult *contracts.Contract
+		err = cResult.Deserialize(result)
+		if err != nil {
+			t.Errorf("Failed to deserialize contract")
+		}
+		if !cResult.Equals(*contract) {
+			t.Error("Result contract does not equal to contract.\nWant: " + cResult.ToString() + "\nGot: " + contract.ToString())
+		}
+	}
 }
 
 func TestGetContractsFromContractsTable(t *testing.T) {
@@ -88,38 +133,38 @@ func TestGetContractsFromContractsTable(t *testing.T) {
 	defer tearDown(dbConn, "testContracts.db")
 
 	senderPrivateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	encodedSenderKey, _ := publickey.Encode(&senderPrivateKey.PublicKey)
+	senderPubKeyHash := hashing.New(encodedSenderKey)
+
 	recipientPrivateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	encodedRecipientKey, _ := publickey.Encode(&recipientPrivateKey.PublicKey)
 	recipientPubKeyHash := hashing.New(encodedRecipientKey)
+
 	var testContracts [](*contracts.Contract)
-
 	contract1, _ := contracts.New(1, senderPrivateKey, recipientPubKeyHash, 1, 1)
-	err := InsertContractIntoContractsTable(contract1)
-	testContracts = append(testContracts, contract1)
-
 	contract2, _ := contracts.New(1, senderPrivateKey, recipientPubKeyHash, 10, 2)
-	err2 := InsertContractIntoContractsTable(contract2)
-	testContracts = append(testContracts, contract2)
+	contract3, _ := contracts.New(1, recipientPrivateKey, senderPubKeyHash, 100, 1)
+	testContracts = append(testContracts, contract1, contract2, contract3)
 
-	if err != nil || err2 != nil {
-		t.Errorf("Failed to insert contract into tableContracts table: %s", err)
+	err := InsertContractsIntoContractsTable(testContracts)
+	if err != nil {
+		t.Errorf("Failed to insert multiple contracts into tableContracts table: %s", err)
 	}
 
 	// Act
 	tableContracts, err := GetContractsFromContractsTable(dbConn, &senderPrivateKey.PublicKey, 1)
 	if err != nil {
-		t.Errorf("Failed to get tableContracts from tableContracts table")
+		t.Errorf("Failed to get contracts from ContractsTable")
 	}
 
 	// Assert
-	if len(tableContracts) != len(testContracts) {
-		t.Errorf("Failed to get the correct number of tableContracts from tableContracts table")
+	if len(tableContracts) != len(testContracts)-1 {
+		t.Errorf("Failed to get the correct number of contracts from ContractsTable")
 	}
 
-	for n := 0; n < len(tableContracts); n++ {
-		result := (*tableContracts[n]).Equals(*testContracts[n])
-		if !result {
-			t.Errorf("Failed to get the expected tableContracts from tableContracts table")
+	for n := 0; n < len(tableContracts)-1; n++ {
+		if !((*tableContracts[n]).Equals(*testContracts[n])) {
+			t.Errorf("Failed to get the expected contracts from ContractsTable")
 		}
 	}
 }
--- internal/sqlstatements/sqlstatements.go
+++ internal/sqlstatements/sqlstatements.go
@@ -6,7 +6,7 @@ const (
 	CREATE_ACCOUNT_BALANCES_TABLE                           = "CREATE TABLE IF NOT EXISTS account_balances (public_key_hash TEXT, balance INTEGER, nonce INTEGER)"
 	CREATE_METADATA_TABLE                                   = "CREATE TABLE IF NOT EXISTS metadata (height INTEGER PRIMARY KEY, position INTEGER, size INTEGER, hash TEXT)"
 	CREATE_PRODUCER_TABLE                                   = "CREATE TABLE IF NOT EXISTS producer (public_key_hash TEXT PRIMARY KEY, timestamp INTEGER)"
-	CREATE_CONTRACT_TABLE                                   = "CREATE TABLE IF NOT EXISTS contracts (serialized_contract TEXT PRIMARY KEY, sender_pub_key TEXT, receipent_pub_key TEXT, nonce INTEGER)"
+	CREATE_CONTRACT_TABLE                                   = "CREATE TABLE IF NOT EXISTS contracts (serialized_contract BINARY PRIMARY KEY, sender_pub_key TEXT, nonce INTEGER)"
 	INSERT_VALUES_INTO_ACCOUNT_BALANCES                     = "INSERT INTO account_balances (public_key_hash, balance, nonce) VALUES(?, ?, ?)"
 	INSERT_VALUES_INTO_METADATA                             = "INSERT INTO metadata (height, position, size, hash) VALUES (?, ?, ?, ?)"
 	INSERT_VALUES_INTO_PRODUCER                             = "INSERT INTO producer (public_key_hash, timestamp) VALUES (?, ?)"
